032bf78d280b262c95093cbfbdb226ceaf42cc69
anupap8958 <82763120+anupap8958@users.noreply.github.com>
2021-04-21 14:48:50 +0700

    Add files via upload

---
+++ platform/src/main/java/com/training/platform/controllers/DemoController.java
@@ -0,0 +1,86 @@
+package com.training.platform.controllers;
+
+import com.training.platform.entities.User;
+import com.training.platform.repositories.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.*;
+
+@RestController
+@RequestMapping(value = "/demo")
+public class DemoController {
+
+    @Autowired
+    private UserRepository userRepository;
+
+//    @GetMapping(value = "")
+//    public String index() {
+//        List<User> users = userRepository.findAll();
+//        System.out.println("############### Find All User (In Console)###############");
+//        System.out.println(users);
+//        String tem = users.toString();
+//        return tem;
+////        return "Method GET, Function : index => SHOW data list on page";
+//    }
+
+    @GetMapping(value = "/{id}")
+    public String showWithPath(@PathVariable String id) {
+        Optional<User> user = userRepository.findById(Integer.parseInt(id));
+        System.out.println("############### Find User By ID (In Console)###############");
+        System.out.println(user);
+        String tem = user.toString();
+        return "Method Get, Function : show, ID : " + id + " => SHOW data by id on page with path";
+    }
+
+    @GetMapping(value = "/index1")
+    public List<User> index1() {
+        List<User> users = userRepository.findByCityAndActiveAndAge("nakornpathom", 1, 18);
+        return users;
+    }
+
+    @GetMapping(value = "/index2")
+    public List<User> index2() {
+        List<Integer> ages = new ArrayList<Integer>(Arrays.asList(18, 19, 22));
+        List<User> users = userRepository.findByAgeIn(ages);
+        return users;
+    }
+
+    // Example for findAllByQuery
+    @GetMapping(value = "/index3")
+    public List<User> test1() {
+        List<User> users = userRepository.findAllByQuery();
+        System.out.println(users);
+        return users;
+    }
+    // Example for findAllByParamsQuery
+//    @GetMapping(value = "/index9")
+//    public List<User> test2() {
+//        return userRepository.findAllByParamsQuery(0, "nakornpathom");
+//    }
+    @GetMapping(value = "/index4")
+    public List<User> test2(@RequestParam String active, @RequestParam String city) {
+        return userRepository.findAllByParamsQuery(Integer.parseInt(active), city);
+    }
+
+    // Example for findAllByJpqlQuery
+    @GetMapping(value = "/index5")
+    public List<User> test3() {
+        return userRepository.findAllByJpqlQuery();
+    }
+    // Example for findAllByJpqlParamsQuery
+//    @GetMapping(value = "/index6")
+//    public List<User> test4() {
+//        return userRepository.findAllByJpqlParamsQuery(0, "bangkok");
+//    }
+
+    @GetMapping(value = "/index7")
+    public List<User> test4(@RequestParam Integer id) {
+        return userRepository.findAllByIdUser(id);
+    }
+
+    @GetMapping(value = "/index8")
+    public List<User> test5(@RequestParam String name) {
+        return userRepository.findAllByNameUser(name);
+    }
+}
---
+++ platform/src/main/java/com/training/platform/controllers/UserController.java
@@ -0,0 +1,94 @@
+package com.training.platform.controllers;
+
+import com.training.platform.entities.User;
+import com.training.platform.services.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.*;
+
+@RestController
+@RequestMapping(value = "/demouser")
+public class UserController {
+    @Autowired
+    private UserService userService;
+
+    @GetMapping(value = "")
+    public List<User> index() throws Exception {
+        return userService.findAll();
+    }
+
+
+    //    @GetMapping(value = "/test1")
+//    public List<User> findAllByJpqlParamsQuery(@RequestParam String active, @RequestParam String city) {
+//        // Change from UserRepository to UserService
+//        return userService.findAllByJpqlParamsQuery(Integer.parseInt(active), city);
+//    }
+    @GetMapping(value = "/test1")
+    public List<User> findAllByJpqlParamsQuery() {
+        // Change from UserRepository to UserService
+        return userService.findAllByJpqlParamsQuery(0, "bangkok");
+    }
+
+    @GetMapping(value = "/test2")
+    public List<User> findAllByJpqlQuery() {
+        return userService.findAllByJpqlQuery();
+    }
+
+//    @GetMapping(value = "/test3")
+//    public List<User> index3(@RequestParam String active, @RequestParam String city) {
+//        return userService.findAllByParamsQuery(Integer.parseInt(active), city);
+//    }
+    @GetMapping(value = "/test3")
+    public List<User> findAllByParamsQuery() {
+       return userService.findAllByParamsQuery(0, "nakornpathom");
+    }
+
+    @GetMapping(value = "/test4")
+    public List<User> findAllByQuery() {
+        return userService.findAllByQuery();
+    }
+
+//    @GetMapping(value = "/test5")
+//    public List<User> findByAgeIn() {
+//        List<Integer> ages = new ArrayList<Integer>(Arrays.asList(20, 21, 22));
+//        List<User> users = userService.findByAgeIn(ages);
+//        return users;
+//    }
+    @GetMapping(value = "/test5")
+    public List<User> findByAgeIn(@RequestBody Map<String ,Object> dataInput) {
+        return userService.findByAgeIn((List<Integer>) dataInput.get("age"));
+    }
+
+//    @GetMapping(value = "/test6")
+//    public List<User> index6(@RequestParam String city,@RequestParam String active,@RequestParam String age) {
+//        return userService.findByCityAndActiveAndAge(city, Integer.parseInt(active), Integer.parseInt(age));
+//    }
+    @GetMapping(value = "/test6/{city}/{active}/{age}")
+    public List<User> findByCityAndActiveAndAge(@PathVariable String city,@PathVariable String active,@PathVariable String age) {
+        return userService.findByCityAndActiveAndAge(city, Integer.parseInt(active), Integer.parseInt(age));
+    }
+
+
+    @GetMapping(value = "/test7")
+    public Page<User> findAllByLimit1(@RequestParam Integer start,@RequestParam Integer limit,@RequestParam  String field) {
+        PageRequest page = PageRequest.of(start, limit, Sort.by(Sort.Direction.ASC, field));
+        return userService.findAllByLimit(start, limit, field);
+    }
+
+    @GetMapping(value = "/test8/{id}")
+    public Optional<User> findById(@PathVariable String id) {
+        Optional<User> user = userService.findById(Integer.parseInt(id));
+        System.out.println("############### Find User By ID (In Console)###############");
+        return user;
+    }
+
+    @GetMapping(value = "/test9")
+    public List<User> findAll() {
+        List<User> users = userService.findAll();
+        return users;
+    }
+}
